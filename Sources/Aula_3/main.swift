a8172ca4ff29c3b131a74741550c1725af9fbe42
AnaLessa07 <analessaferreira.aa@gmail.com>
2025-09-03 18:33:28 +0000

    alterei codigo

--- Sources/Aula_3/main.swift
+++ Sources/Aula_3/main.swift
@@ -1,3 +1,41 @@
 import Foundation
+class Carro {
+    private (set) var marca: String
+    private (set) var modelo: String
 
-print("Aula 3 de Swift!!")
+    init (marca: String, modelo: String) {
+        self.marca = marca
+        self.modelo = modelo
+    }
+
+    public func acelerar(){
+        print("O carro esta acelerado")
+    }
+
+    public func frear(){
+        print("o carro frear")
+    }
+
+
+    public func getMarca() -> String{
+        return self.marca
+    }
+
+    private func setMarca(marca: String){
+        self.marca = marca
+    }
+
+    public func alterarM(senha: Int, novaM: String) {
+        if senha == 123 {
+            self.setMarca(marca: novaM)
+        }
+    }
+
+}
+let carro = Carro(marca: "Ford", modelo: "")
+
+print()
+print(carro.modelo)
+
+carro.alterarM(senha:123, novaM:"cheveront")
+print(carro.marca)
