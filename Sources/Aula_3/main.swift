7c4631a705a08f725c2d460153113deb83f5a679
AnaLessa07 <analessaferreira.aa@gmail.com>
2025-09-05 13:43:48 +0000

    treinando

--- Sources/Aula_3/main.swift
+++ Sources/Aula_3/main.swift
@@ -46,5 +46,38 @@ print(carro.marca) */
 // ATIVIDADE 2 //
 
 class PerfilUsuario {
-    
+    public let id: UUID
+    public let dataDeCadastro: Date
+    public var nomeDeExibicao: String
+    private(set) var hashDaSenha: String
+
+
+    init (nomeDeExibicao: String, senha: String){
+        self.id = UUID()
+        self.dataDeCadastro = Date()
+        self.nomeDeExibicao = nomeDeExibicao
+        self.hashDaSenha = senha + "_hashed"
+    }
+
+    public func alterarSenha (novaSenha: String) -> Bool{
+        if novaSenha.count < 8 {
+            print("A nova senha deve ter pelo menos 8 caracteres.")
+            return false
+        }
+        self.hashDaSenha = novaSenha + "_hashed"
+        return true
+    }
+
+    public func autenticar (senha: String) -> Bool{
+        let hashear = senha + "_hashed"
+        if hashDaSenha == hashear{
+            return true
+        } else{
+            return false
+        }
+    }
 }
+
+let usuario = PerfilUsuario(nomeDeExibicao: "Alex", senha: "senhaSegura123")
+print("Login com senha correta: \(usuario.autenticar(senha: "senhaSegura123"))")
+// Esperado: true
