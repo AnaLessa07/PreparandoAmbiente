44f05d51e8cfb65ca11620116a54ee473bf10d1c
AnaLessa07 <analessaferreira.aa@gmail.com>
2025-09-05 18:52:49 +0000

    treinando em aula

--- Sources/Aula_5/main.swift
+++ Sources/Aula_5/main.swift
@@ -1,3 +1,41 @@
 import Foundation
 
-print("Aula 5 de Swift!!")
+protocol Rastreavel {
+    func obterLocalização() -> String
+}
+
+class Veiculo {
+    var velocidade: Double = 0.0
+
+    init (velocidade: Double){
+        self.velocidade = velocidade
+        print("Veiculo acelerando para \(velocidade) km/h")
+    }
+
+    func acelerar(){
+        velocidade += 10
+    }
+}
+
+class CarroDeEntrega: Veiculo, Rastreavel {
+    var placa: String
+
+    init (placa: String, velocidade: Double){
+        self.placa = placa
+        super.init(velocidade:velocidade)
+    }
+
+    override func acelerar() {
+        velocidade += 5
+    }
+
+    func obterLocalização() -> String {
+        return "O carro com a placa \(placa) esta na rua dos bobos"
+    }
+
+}
+
+let Carro = CarroDeEntrega(placa: "siiej9", velocidade: 50)
+Carro.acelerar()
+Carro.obterLocalização()
+
